b85e317abb30c9c56268f5116c21722b5a8cf516
Jonathan Tavares <jonathantavares.all@gmail.com>
2024-08-13 08:05:24 -0300

    fix: add index name in env vars

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_pinecone import PineconeVectorStore
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-
-index_name = "tes-knowledge-database"  # change if desired
 
 # Load the document, split it into chunks, and embed each chunk.
 loader = PyPDFLoader("database/manual_skyrim.pdf")
@@ -16,4 +15,4 @@ text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 docs = text_splitter.split_documents(pages)
 
 embeddings = OpenAIEmbeddings()
-store = PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name)
+store = PineconeVectorStore.from_documents(docs, embeddings, index_name=os.getenv('INDEX_NAME'))
